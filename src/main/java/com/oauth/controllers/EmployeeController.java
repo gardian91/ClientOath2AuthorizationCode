a54eb0d4e1e693d583ec4fe396d49ef85c9bcd42
Gardian Wojton <someone@nowhere.com>
2019-06-28 16:15:53 +0200

    some thing added

--- src/main/java/com/oauth/controllers/EmployeeController.java
+++ src/main/java/com/oauth/controllers/EmployeeController.java
@@ -17,9 +17,12 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,34 +31,44 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oauth.model.Employee;
 
-@Controller
+@RestController
 public class EmployeeController {
 
 	@RequestMapping(value = "/getEmployees", method = RequestMethod.GET)
 	public ModelAndView getEmployeeInfo() {
+		
 		return new ModelAndView("getEmployees");
+		//return new ModelAndView("getEmployees");//////////////////////////////////////////
+		
+		/*
+		 * RestTemplate restTemplate = new RestTemplate(); String url
+		 * ="http://localhost:8080/oauth/authorize"; HttpHeaders headers = new
+		 * HttpHeaders(); headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		 * 
+		 * MultiValueMap<String, String> map = new LinkedMultiValueMap<String,
+		 * String>(); map.add("response_type", "code");
+		 * map.add("client_id","javainuse");
+		 * map.add("redirect_uri","http://localhost:8090/showEmployees");
+		 * map.add("scope", "read");
+		 * 
+		 * HttpEntity<MultiValueMap<String, String>> request = new
+		 * HttpEntity<MultiValueMap<String, String>>(map, headers);
+		 * 
+		 * ResponseEntity<String> response = restTemplate.postForEntity(url ,
+		 * request,String.class);
+		 * 
+		 * return new ModelAndView("redirect:/showEmployees");
+		 */
+		
 	}
 
 	@RequestMapping(value = "/showEmployees", method = RequestMethod.GET)
-	public ModelAndView showEmployees(@RequestParam("code") String code) throws JsonProcessingException, IOException {
+	public Employee[] showEmployees(@RequestParam("code") String code) throws JsonProcessingException, IOException {
 		ResponseEntity<String> response = null;
 		System.out.println("Authorization Ccode------" + code);
 
 		RestTemplate restTemplate = new RestTemplate();
 
-		
-		/*
-		 * List<HttpMessageConverter<?>> messageConverters = new
-		 * ArrayList<HttpMessageConverter<?>>(); MappingJackson2HttpMessageConverter
-		 * converter = new MappingJackson2HttpMessageConverter();
-		 * converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
-		 * messageConverters.add(converter); messageConverters.add(new
-		 * StringHttpMessageConverter()); messageConverters.add(new
-		 * AllEncompassingFormHttpMessageConverter());
-		 * restTemplate.setMessageConverters(messageConverters);
-		 */
-		 
-		
 		String credentials = "javainuse:secret";
 		String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
 
@@ -86,12 +99,10 @@ public class EmployeeController {
 		headers1.add("Authorization", "Bearer " + token);
 		HttpEntity<String> entity = new HttpEntity<>(headers1);
 
-		ResponseEntity<String> employees = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+		ResponseEntity<Employee[]> employees = restTemplate.exchange(url, HttpMethod.GET, entity, Employee[].class);
 		System.out.println(employees);
-		String employee = employees.getBody();
+		Employee[] employee = employees.getBody();
 
-		ModelAndView model = new ModelAndView("showEmployees");
-		model.addObject("listValue", employee);
-		return model;
+		return employee;
 	}
 }
